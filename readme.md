1a4fe816f9d530380cf8dab81bbb3ccf7dd44271
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-26 23:43:10 +0100

    Installed my own react boilerplate

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Expensify App
+
+A React App for managing expenses and budget.
+
+Full docs to follow.
+
+Demo version: 
+
+## Run the app locally
+
+### Prerequisites
+
+Install [Node.js](https://nodejs.org/en/).
+
+### Getting started
+
+Fork this repository then `git clone` your new repo to get a local installation.
+
+`cd` into your new project folder then `yarn` to install all package dependencies.
+
+...
+
+## Features:
+- ...
+- ...
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' }); 
+    
+    console.log(env);
+    return {
+        entry: './src/app.js', 
+        output: {
+            path: path.join(__dirname, 'public'), 
+            filename: 'bundle.js'
+        }, 
+        "mode": "development", 
+        module: {
+            rules: [{
+                loader: 'babel-loader', 
+                test: /\.js$/, 
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/, 
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        }, 
+        plugins: [
+            CSSExtract
+        ], 
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', 
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    }
+}
