34d204f89639d4aa11081cddc4121b8e9da3cc99
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2020-05-23 13:14:22 +0100

    Adds moment, prettier; wires up Expense Form

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => (
+const EditExpensePage = (props) => {
+  //console.log(props); // includes 'expense' key
+  return (
     <div>
-        Edit expense component {props.match.params.id}
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.history.push('/');
+        }}
+      >
+        Remove
+      </button>
     </div>
-);
+  );
+};
 
-export default EditExpensePage;
+// give component (above) access to the current expense object
+// we can access the current props passed into our HOC as the 2nd argument
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => {
+      return expense.id === props.match.params.id;
+    }),
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
